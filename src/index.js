80224ce57da7ea1942d448b0955835b49ccc8091
JeevanGowdaSK <jeevangowdru23@gmai.com>
2025-04-02 21:19:36 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require('dotenv').config({path: './env'})
+
+import path from "path";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+// the below written is an IIFE(Immediately Invoked Function Expression) which is used to connect to the database
+// the ";" at the start is used to prevent errors in case the previous file does not end with a semicolon
+// the async function is used to connect to the database and the await keyword is used to wait for the connection to be established
+// the try-catch block is used to handle errors that may occur during the connection process
+;(async() => {
+    try{
+         await  mongoose.connect('${process.env.MONGO_URI}/${DB_NAME}')
+         app.on("error",() => {
+            console.log("Error connecting to database")
+            throw error
+        })
+
+        app.listen(process.env.PORT,() => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        }
+        )
+    }catch(error){
+        console.log("Error connecting to database", error)
+        throw error
+    }
+})()
+*/
